2b86ef5bc86d8b5824510713abcad23bc205ac46
HarshVDS <meet2swami@gmail.com>
2025-06-26 15:48:58 +0530

    update-l

--- Temple-backend/server.js
+++ Temple-backend/server.js
@@ -18,8 +18,7 @@ app.use(express.json())
 app.use(cors({
     origin: 'https://iscon-f.vercel.app',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+
 }))
 // dotenv.config()
 
