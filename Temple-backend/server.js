b976367098e2e9d160e917824eb13f345da9e152
HarshVDS <meet2swami@gmail.com>
2025-06-26 15:32:34 +0530

    again updated

--- Temple-backend/server.js
+++ Temple-backend/server.js
@@ -15,7 +15,12 @@ const port = process.env.PORT || 4000
 connectDB()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://iscon-f.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 // dotenv.config()
 
 app.use('/api/users', userRouter)
