03c5aff2adf4a7996e442d28dde31db70cfca81a
Harshvardhan Swami <134095085+HarshVDS@users.noreply.github.com>
2025-06-27 17:00:36 +0530

    Delete Temple-backend directory

--- Temple-backend/routes/approvalRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getPendingDisciple, approveDisciple } from '../controllers/approvalController.js';
-
-const approvalRouter = express.Router();
-
-approvalRouter.get('/verify/:token', getPendingDisciple);
-approvalRouter.post('/approve', approveDisciple);
-export default approvalRouter;
